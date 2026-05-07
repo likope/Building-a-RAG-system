7a7a4aa13fc4981d55a0cb81ceb79b1fe5f51e2e
User <you@example.com>
2026-05-07 20:41:04 +0200

    update

--- main.py
+++ main.py
@@ -12,13 +12,19 @@ ollama_url = "http://localhost:11434"
 #definisco il modello di chat
 model_llm = "assistente"
 
+#definisco la dict state che conterrà i parametri della conversazione
+state = {
+    "input" : "user input",
+    "output": "model output",
+    "history": "conversation history"
+}
+
 
 #definisco i parametri del modello di chat su ollama
 llm = ChatOllama(
     model = model_llm,
     temperature = 0.5,
     base_url = ollama_url,
-    verbose = False
     )
 
 
@@ -28,7 +34,12 @@ def chat_with_llm (input: str = test_input) -> str:
     return risposta
 
 
+#definisco una f che prende l input e output e costruisce la history relativa nel state
+def update_history(state: dict = state) -> dict:
+    return state["history"] + "User input: " + state["input"] + "Model output: " + state["output"]
+
+
 if __name__ == "__main__":
     #testo la f
-    output = chat_with_llm()
-    print(output)
+    chain = RunnableLambda(chat_with_llm) | RunnableLambda(update_history)
+    print(state)
