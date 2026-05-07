9032182af53595a930b10ac080eeb3b3dea39621
likope <massimo.domenici06@gmail.com>
2026-05-07 18:04:50 +0100

    update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from langchain_ollama import ChatOllama
 
 
 #definisco un input generico di test
-test_input = "Hello, what are you?"
+test_input = "Hello, who are you?"
 
 #definisco l'url del server Ollama
 ollama_url = "http://localhost:11434"
@@ -16,11 +16,19 @@ model_llm = "assistente"
 #definisco i parametri del modello di chat su ollama
 llm = ChatOllama(
     model = model_llm,
-    temperature = 0.2,
-    base_url = ollama_url
-)
+    temperature = 0.5,
+    base_url = ollama_url,
+    verbose = False
+    )
 
 
 #definisco una f che prende un input str e lo passa al modello di chat su ollama, restituendo un output str
-def llm (input: str) -> str:
-    return llm(input)
+def chat_with_llm (input: str = test_input) -> str:
+    risposta = llm.invoke(test_input)
+    return risposta
+
+
+if __name__ == "__main__":
+    #testo la f
+    output = chat_with_llm()
+    print(output)
