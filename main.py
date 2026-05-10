30b936fc2a0e478486fb4a245c6eccbb60aac6e0
likope <massimo.domenici06@gmail.com>
2026-05-10 12:36:29 +0100

    assistente con history

--- main.py
+++ main.py
@@ -1,45 +1,24 @@
 #Import
-from langchain_core.runnables import RunnableLambda, RunnableAssign, StrOutputParser
+from langchain_core.runnables import RunnableLambda, RunnableAssign
+from langchain_core.output_parsers import StrOutputParser
 from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
+from config import state, prompt, llm
+from f import update_history
 
-
-#definisco un input generico di test
-test_input = "Hello, who are you?"
-
-#definisco l'url del server Ollama
-ollama_url = "http://localhost:11434"
-
-#definisco il modello di chat
-model_llm = "assistente"
-
-#definisco la dict state che conterrà i parametri della conversazione
-state = {
-    "input" : test_input,
-    "output": "",
-    "history": ""
-}
-
-
-#definisco i parametri del modello di chat su ollama
-llm = ChatOllama(
-    model = model_llm,
-    temperature = 0.5,
-    base_url = ollama_url,
-    )
-
-
-#definisco una f che prende l input e output e costruisce la history relativa nel state
-def update_history(state: dict) -> str:
-    return state["history"] + f"User input: {state['input']}, Assistant output: {state['output']}"
 
 
 #determino la chain
 chain = (
-    RunnableAssign({"output": llm | StrOutputParser() }) | RunnableAssign({"history": update_history})
+    RunnableAssign({"output": prompt | llm | StrOutputParser() }) | RunnableAssign({"history": update_history})
 )
 
 
 if __name__ == "__main__":
-    #eseguo la chain
-    result = chain.invoke(state)
-    print(result)
+    while True:
+        user_input = input("Scrivi il prompt o digita 'exit' per uscire: \n")
+        if user_input.lower() == "exit":
+            break
+        state["input"] = user_input
+        result = chain.invoke(state)
+        print(result)
